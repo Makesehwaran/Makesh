b078d838ebcb68b48f9a7d25290a8f3bc3d0f44f
Makesehwaran <35652471+Makesehwaran@users.noreply.github.com>
2018-01-30 16:31:29 +0530

    Create vowel.c

---
+++ vowel.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char let;
+clrscr();
+printf("enter the letter:",let);
+scanf("%c",&let);
+{
+if(let=='a')
+printf("%c is a vowel:",let);
+else if(let=='e')
+printf("%c is avowel:",let);
+else if(let=='o')
+printf("%c is a vowel:",let);
+else if(let=='u')
+printf("%c is a vowel:",let);
+else
+printf("%c is a constant",let);
+}
+}
