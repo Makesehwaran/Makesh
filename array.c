f93582827598fcd7ee7a2b976a1cafa5f5f3930d
Makesehwaran <35652471+Makesehwaran@users.noreply.github.com>
2018-01-30 16:38:16 +0530

    Create array.c

---
+++ array.c
@@ -0,0 +1,14 @@
+#include<include.h>
+#include<conio.h>
+void main()
+{
+int a,i,total=0;
+printf(""enter the values of a");
+scanf("%d",&a);
+for(i=0;i<a;i++)
+{
+total=total+a;
+{
+printf("sum is %d",total);
+getch();
+}
