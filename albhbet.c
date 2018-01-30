6328f580671beae689abc8f21863ff7c2ec81bb9
Makesehwaran <35652471+Makesehwaran@users.noreply.github.com>
2018-01-30 16:18:44 +0530

    Create albhbet.c

---
+++ albhbet.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char ch;
+printf("enter the letter:"ch);
+scanf("%d",ch);
+{
+if((ch>65 && ch <=90)||(ch>=92 && ch <=122))
+printf("%c is a not alphabet",ch);
+return 0;
+}
