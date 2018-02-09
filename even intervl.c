9529117b6be177d8553bfd0c2a8fe242de3be992
Makesehwaran <35652471+Makesehwaran@users.noreply.github.com>
2018-02-09 16:00:14 +0530

    Create even intervl.c

---
+++ even intervl.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n,k,l;
+printf("enter the values of intervals are:");
+scanf("%d%d",&k,&l);
+for(n=0;n<6;n++)
+{
+if(n%2=0)
+{
+printf("odd numbers are",&n);
+}
+}
+getch();
+}
