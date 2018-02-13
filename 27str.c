d779a32e627781f5900af0c34f29dd874703e769
Makesehwaran <35652471+Makesehwaran@users.noreply.github.com>
2018-02-13 16:30:43 +0530

    Create 27str.c

---
+++ 27str.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<math.h>
+void main()
+{
+int n;
+clrscr();
+printf("enter number");
+scanf("%d",&n);
+if(isdigit(n));
+{
+printf("yes");
+}
+else
+{
+printf("no");
+}
+}
