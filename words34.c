2d0ee694816017e4a7a46ea3def4b2938cca390f
Makesehwaran <35652471+Makesehwaran@users.noreply.github.com>
2018-03-02 14:07:51 +0530

    Create words34.c

---
+++ words34.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main() {
+    char a[100];
+    int i,count=0;
+    scanf("%s",a);
+    for(i=0;i<100;i++)
+       {
+           if(a[i]=='.')
+           count++;
+       }
+       printf("%d\twords",count);
+}
