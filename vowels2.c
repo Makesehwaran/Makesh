7ea7063b1e991940bc72ab3685923ce0741d8794
Makesehwaran <35652471+Makesehwaran@users.noreply.github.com>
2018-03-20 16:42:47 +0530

    Create vowels2.c

---
+++ vowels2.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+char str[10],i;
+    if(str[i]=='a',str[i]=='e',str[i]=='i',str[i]=='o',str[i]=='u')
+    {
+        printf("vowels");
+    }
+    else
+    {
+        printf("no");
+    }
+    getch();
+}
