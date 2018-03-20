5ea1ed43411909a622ef88714fce976941a7cd74
Makesehwaran <35652471+Makesehwaran@users.noreply.github.com>
2018-03-20 16:38:44 +0530

    Create palintrome.c

---
+++ palintrome.c
@@ -0,0 +1,14 @@
+    #include<stdio.h>
+    void main()
+    {
+    char str[10]="amma",i;
+    if(str[i]=='a',str[i]=='m',str[i]=='m',str[i]=='a')
+    {
+        printf("yes");
+    }
+    else
+    {
+        printf("no");
+    }
+    getch();
+}
